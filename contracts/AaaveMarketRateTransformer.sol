77e4cb213c2b24fef6102f64f5ad4d44f7176adc
mkflow27 <mkflow27@gmail.com>
2024-12-04 17:24:26 +0100

    refactor: public and virtual

---
+++ contracts/AaaveMarketRateTransformer.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.8.0;
+
+import "./ERC4626RateProviderBase.sol";
+
+import "./interfaces/IRateProvider.sol";
+import "@openzeppelin/contracts/interfaces/IERC4626.sol";
+
+contract AaaveMarketRateTransformer is ERC4626RateProviderBase {
+    address public vaultAssetFeed;
+
+    constructor(address _vaultAssetFeed, address _erc4626Vault) 
+    ERC4626RateProviderBase(IERC4626(_erc4626Vault))
+    {
+        vaultAssetFeed = _vaultAssetFeed;
+    }
+
+    function getRate() public view override returns (uint256) {
+        // super.getRate returns an 18 decimal fixed point number
+        return (super.getRate() * IRateProvider(vaultAssetFeed).getRate()) / 1e18;
+    }
+}
---
+++ contracts/AaaveMarketRateTransformerFactory.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.8.0;
+
+import "./BaseRateProviderFactory.sol";
+import "./AaaveMarketRateTransformer.sol";
+
+/**
+ * @title  Aave Market Rate Transformer Factory
+ * @notice This contract is a factory for creating instances of AaveMarketRateTransformer.
+ * @dev    This factory contract allows for the deployment of AaveMarketRateTransformer contracts, 
+ *         which are used to transform market rates from the Aave protocol.
+ */
+
+
+contract AaaveMarketRateTransformerFactory is BaseRateProviderFactory {
+    /**
+     * @notice Deploys a new AaveMarketRateTransformer contract using a price feed.
+     * @param _vaultAssetFeed - The Aave price feed contract.
+     * @param _erc4626Vault - The ERC4626 vault contract.
+     */
+    function create(address _vaultAssetFeed, address _erc4626Vault) external returns (AaaveMarketRateTransformer) {
+        AaaveMarketRateTransformer rateProvider = new AaaveMarketRateTransformer(_vaultAssetFeed, _erc4626Vault);
+        _onCreate(address(rateProvider));
+        return rateProvider;
+    }
+}
